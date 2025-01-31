03cc0434dff7a7f7f87337420e9f3b2c13c0325c
krishan190 <krishnanamdeo2001@gmail.com>
2025-01-31 23:30:08 +0530

    db commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect("mongodb+srv://user:user123@cluster0.fn9be.mongodb.net/ecomcrud?retryWrites=true&w=majority&appName=Cluster0");
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
